55152bce40dbae2063db3da81a757babc00d6e55
slightlycyborg <collinalexbell@gmail.com>
2020-05-14 00:13:53 +0000

    added path

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -10,3 +10,6 @@ type Wallet interface {
 	SendAmtTo(amt float64, address Address) Tx
 	Balance() float64
 }
+
+// see: https://medium.com/myetherwallet/hd-wallets-and-derivation-paths-explained-865a643c7bf2
+var path string = "m/44'/60'/0'/0"
