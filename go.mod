db070e236bdf846185f5963ebc17c4e7fc916636
slightlycyborg <collinalexbell@gmail.com>
2020-05-14 00:34:11 +0000

    added go-ethereum as dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kuberlog/wallet
 
 go 1.14
+
+require github.com/ethereum/go-ethereum v1.9.14
--- wallet/impl/eth.go
+++ wallet/impl/eth.go
@@ -1,4 +1,11 @@
 package eth
 
+import "github.com/ethereum/go-ethereum/accounts"
+
+var DefaultRootDerivationPath = accounts.DefaultRootDerivationPath
+
 // see: https://medium.com/myetherwallet/hd-wallets-and-derivation-paths-explained-865a643c7bf2
 var path string = "m/44'/60'/0'/0"
+
+type Wallet struct {
+}
